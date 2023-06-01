6d9d8e962f67a180c981b6a09dd678305e9f0e86
AngeloDima <angelodimauro91@gmail.com>
2023-06-01 17:13:02 +0200

    mini scafolding e aggiunta delle 2 rotte Home e Previsioni

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { PrevisioniComponent } from './home/previsioni/previsioni.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "", component: HomeComponent },
+  { path: "previsioni", component: PrevisioniComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
