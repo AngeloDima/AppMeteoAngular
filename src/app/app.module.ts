7b3c85a9dd30063503ccc98ab111fd08cbf4de57
AngeloDima <angelodimauro91@gmail.com>
2023-06-04 15:40:18 +0200

    levo la NavBar (inutile)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
-import { NavBarComponent } from './home/nav-bar/nav-bar.component';
 import { PrevisioniComponent } from './home/previsioni/previsioni.component';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
@@ -13,7 +12,6 @@ import { HttpClientModule } from '@angular/common/http';
   declarations: [
     AppComponent,
     HomeComponent,
-    NavBarComponent,
     PrevisioniComponent
   ],
   imports: [
